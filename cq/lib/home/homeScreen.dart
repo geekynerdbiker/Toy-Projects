d338d52d2e0410f412e0ccbf5a9a2b85e2f70b11
Jacob An <lightyear.thespaceranger@gmail.com>
2020-08-11 00:00:55 +0900

    Update home

--- cq/lib/home/homeScreen.dart
+++ cq/lib/home/homeScreen.dart
@@ -1,45 +1,104 @@
+import 'package:cq/contents/classDetailPage.dart';
+import 'package:cq/contents/stone/stoneDetailPage.dart';
+import 'package:cq/database/classes.dart';
+import 'package:cq/database/contents.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Hasla Dictionary',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0.5,
+      ),
+      backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          classCardList(context),
+          Column(
+            children: [
+              classCardList(context),
+              contentCardList(context),
+            ],
+          ),
+          copyright(context),
         ],
       ),
     );
   }
 
   Widget classCardList(BuildContext context) {
-    return Expanded(
-      child: GridView.count(
-        mainAxisSpacing: 5,
-        crossAxisSpacing: 5,
-        padding: EdgeInsets.all(10),
-        crossAxisCount: 3,
-        children: [
-          Container(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.black,
-          ),
-        ],
+    return Container(
+      height: MediaQuery.of(context).size.width * 0.7,
+      child: GridView.builder(
+        padding: EdgeInsets.all(15),
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemCount: 6,
+        itemBuilder: (context, index) {
+          return classCard(context, classItems[index], index);
+        },
+      ),
+    );
+  }
+
+  Widget classCard(BuildContext context, ClassItem classItem, int index) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ClassDetailPage(index)));
+      },
+      child: Container(
+        margin: EdgeInsets.all(5),
+        color: Colors.grey,
+        child: Text(classItem.title),
+      ),
+    );
+  }
+
+  Widget contentCardList(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.width * 0.5,
+      child: GridView.builder(
+        padding: EdgeInsets.all(15),
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 2.5),
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return contentCard(context, contentItems[index]);
+        },
+      ),
+    );
+  }
+
+  Widget contentCard(BuildContext context, ContentItem contentItem) {
+    return InkWell(
+      onTap: () {
+        if(contentItem.title == 'Stones')
+          Navigator.push(context, MaterialPageRoute(builder: (context) => StoneDetailPage()));
+      },
+        child: Container(
+      margin: EdgeInsets.all(5),
+      color: Colors.grey,
+      child: Text(contentItem.title),
+        ),
+    );
+  }
+
+  Widget copyright(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20),
+        child: Text(
+          'DEVELOPER: lightyear.thespaceranger@gmail.com',
+          style: TextStyle(fontSize: 12, color: Colors.grey[400]),
+        ),
       ),
     );
   }
