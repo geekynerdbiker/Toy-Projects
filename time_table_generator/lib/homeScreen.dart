df91b00664f4f03c90ef044521f25140c365b57d
Jacob An <lightyear.thespaceranger@gmail.com>
2020-08-13 16:48:10 +0900

    Update time table generator

---
+++ time_table_generator/lib/homeScreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:time_table_generator/createPage.dart';
+import 'package:time_table_generator/loadPage.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Time Table Home'),
+          backgroundColor: Colors.white,
+          elevation: 0.6,
+        ),
+        backgroundColor: Colors.white,
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => CreatePage()));
+                },
+                child: Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Text('Create'),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => LoadPage()));
+                },
+                child: Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  child: Text('Load'),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ time_table_generator/lib/loadPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoadPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.yellow,),
+    );
+  }
+}
